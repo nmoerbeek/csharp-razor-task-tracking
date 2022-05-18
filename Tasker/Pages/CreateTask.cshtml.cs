eb00bb2f03be17447d2efe040add0a30fca4759d
Nmoerbeek <nmoerbeek@gmail.com>
2022-05-18 05:51:52 -0400

    Creating the Layout and Navigation

---
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tasker.Pages
+{
+    public class CreateTaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
