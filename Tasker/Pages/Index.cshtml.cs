b8e06237b4a86c763d122570eefbe771f178c1af
Nmoerbeek <nmoerbeek@gmail.com>
2022-05-19 05:56:47 -0400

    Display Data

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+       private ApplicationDbContext _context;
+        public IEnumerable<Task> Tasks { get; set; }
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
