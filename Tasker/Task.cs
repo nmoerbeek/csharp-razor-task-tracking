f10f6fb421b491b792f5b162bb8d2895572b06c0
Nmoerbeek <nmoerbeek@gmail.com>
2022-05-19 06:05:23 -0400

    Data Validation

--- Tasker/Task.cs
+++ Tasker/Task.cs
@@ -1,10 +1,18 @@
-﻿namespace Tasker
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasker
 {
     public class Task
     {
+       
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [MinLength(10)]
+        [Required ]
         public string Description { get; set; }
+        [Range(1,5)]
+        [Required]
         public int Priority { get; set; }
     }
 }
